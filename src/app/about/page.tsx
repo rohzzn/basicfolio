9849afd9eeb350d9b2305a088ebb7464863aadb1
Rohan <rohansanjeev6464@gmail.com>
2024-12-24 22:04:58 -0500

    feat: enhance about page with social media links and update personal description

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react';
 import Link from 'next/link';
-import { FaArrowRight, FaEnvelope } from 'react-icons/fa';
+import { FaArrowRight, FaEnvelope, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Home: React.FC = () => (
   <div className="max-w-7xl">
@@ -11,24 +11,46 @@ const Home: React.FC = () => (
         I&#39;m currently pursuing my master&#39;s in Computer Science at the University of Cincinnati. Previously, I worked as a Software Development Engineer at Ixigo.
       </p>
       <p className="text-zinc-600 dark:text-zinc-400">
-        Over the past few years, I&#39;ve immersed myself in the digital world—building side projects, conducting independent research, and delving deep into understanding how things work. This journey has fueled my passion for technology and innovation.
+        Over the years, I&#39;ve immersed myself in software engineering—building side projects, doing independent research, and exploring how things work under the hood. This passion for technology and innovation motivates me to create solutions that make a difference.
       </p>
       <p className="text-zinc-600 dark:text-zinc-400">
-        Beyond coding, I enjoy exploring new technologies, reading about the latest trends in software development, and collaborating with like-minded individuals to bring ideas to life. I&#39;m dedicated to creating solutions that make a meaningful impact and continuously strive to grow both personally and professionally in the ever-evolving tech landscape.
-      </p>
-      <p className="text-zinc-600 dark:text-zinc-400">
-        In my free time, I love experimenting with different programming languages and frameworks, which allows me to stay versatile and adaptable. Whether it&#39;s contributing to open-source projects or developing my own applications, I find immense satisfaction in problem-solving and overcoming challenges.
-      </p>
-      <p className="text-zinc-600 dark:text-zinc-400">
-        I&#39;m also passionate about mentoring and sharing knowledge. Guiding aspiring developers and helping them navigate their careers brings me joy and reinforces my own understanding of complex concepts. Building a strong community is essential, and I believe in the power of collective growth and learning.
-      </p>
-      <p className="text-zinc-600 dark:text-zinc-400">
-        My fascination with artificial general intelligence (AGI) lies in its potential to create systems that can understand and interact with the world in ways similar to human cognition. Engaging with AGI challenges me to explore the depths of machine learning and cognitive computing, fostering a deeper appreciation for the complexities of intelligence.
+        I&#39;m also passionate about mentoring fellow developers and contributing to open-source communities. My curiosity about artificial general intelligence (AGI) continuously shapes my thinking about the future of cognitive computing.
       </p>
       <br />
     </div>
 
     <div className="mt-6 flex space-x-4">
+      {/* Twitter */}
+      <a
+        href="https://twitter.com/rohzzn"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-flex items-center justify-center bg-zinc-100 dark:bg-zinc-800 text-zinc-700 dark:text-zinc-300 px-6 py-3 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+      >
+        Twitter
+        <FaTwitter className="ml-2" />
+      </a>
+      {/* LinkedIn */}
+      <a
+        href="https://linkedin.com/in/rohzzn  "
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-flex items-center justify-center bg-zinc-100 dark:bg-zinc-800 text-zinc-700 dark:text-zinc-300 px-6 py-3 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+      >
+        LinkedIn
+        <FaLinkedin className="ml-2" />
+      </a>
+      {/* GitHub */}
+      <a
+        href="https://github.com/rohzzn"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="inline-flex items-center justify-center bg-zinc-100 dark:bg-zinc-800 text-zinc-700 dark:text-zinc-300 px-6 py-3 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+      >
+        GitHub
+        <FaGithub className="ml-2" />
+      </a>
+      {/* Resume */}
       <Link
         href="/stack"
         className="inline-flex items-center justify-center bg-zinc-100 dark:bg-zinc-800 text-zinc-700 dark:text-zinc-300 px-6 py-3 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors duration-200 ease-in-out transform hover:scale-105 active:scale-95"
@@ -36,6 +58,7 @@ const Home: React.FC = () => (
         Resume
         <FaArrowRight className="ml-2" />
       </Link>
+      {/* Contact */}
       <a
         href="mailto:pothurrs@mail.uc.edu"
         className="inline-flex items-center justify-center bg-zinc-100 dark:bg-zinc-800 text-zinc-700 dark:text-zinc-300 px-6 py-3 rounded-lg hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors duration-200 ease-in-out transform hover:scale-105 active:scale-95"
