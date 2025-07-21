4e4fe2b76ced16877c3eb6311e184c148d4f0626
Rohan <rohan.pothuru@cchmc.org>
2025-07-21 11:50:39 -0400

    small change

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -45,7 +45,7 @@ const Stack = () => {
     {
       company: 'Abhibus (Ixigo)',
       role: 'Software Development Engineer',
-      duration: 'June 2023 - September 2023',
+      duration: 'June 2023 - June 2024',
     },
   ];
 
